893099a562137854e132bf740151e0eb841bf77e
Ian Walker-Sperber <ianwsperber@gmail.com>
2014-02-14 14:31:05 -0800

    Initial Commit

---
+++ SimpleSprite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleSprite
+//
+//  Created by Ian Walker-Sperber on 2/14/14.
+//  Copyright (c) 2014 Ian Walker-Sperber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleSprite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleSprite
+//
+//  Created by Ian Walker-Sperber on 2/14/14.
+//  Copyright (c) 2014 Ian Walker-Sperber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
